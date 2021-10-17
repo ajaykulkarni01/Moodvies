f2223aa118c2db1f80d2b46557b2334c659b14c5
Ajay K <ajaycoolkarni@yahoo.com>
2021-10-17 16:10:22 +1030

    All files added

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.carousel').carousel({
+    fullWidth: true,
+    // indicators: true,
+  });
+  $(document).ready(function(){
+    $('.carousel').carousel();
+    // $('.carousel').width(600);
+    // $('.carousel').height(250);
+  });
